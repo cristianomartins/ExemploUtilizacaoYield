bf883b2c854f4324d86574ec17fd0fd4dda56ed2
cristianomartins <cmainfor@gmail.com>
2015-04-16 00:07:59 -0300

    Exemplo de utilização da keyword Yield

--- ExemploDeUtilizacaoDeYield.Console/Program.cs
+++ ExemploDeUtilizacaoDeYield.Console/Program.cs
@@ -4,12 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExemploDeUtilizacaoDeYield.Console
+namespace ConsoleApplication1
 {
-    class Program
+    public class Cliente
     {
-        static void Main(string[] args)
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+    }
+
+    public class CntrCliente
+    {
+        public IEnumerable<Cliente> ListaClientesAcimaDe20Anos()
         {
+            List<Cliente> BancoDeDados = new List<Cliente>()
+            {
+                new Cliente() {Id = 1, Nome = "Cliente 1", Idade = 40 },
+                new Cliente() {Id = 2, Nome = "Cliente 2", Idade = 20 },
+                new Cliente() {Id = 3, Nome = "Cliente 3", Idade = 16 },
+                new Cliente() {Id = 4, Nome = "Cliente 4", Idade = 26 },
+                new Cliente() {Id = 5, Nome = "Cliente 5", Idade = 28 },
+                new Cliente() {Id = 7, Nome = "Cliente 6", Idade = 22 }
+            };
+
+            foreach (Cliente cliente in BancoDeDados)
+            {
+                if (cliente.Idade > 20)
+                    yield return cliente;
+            }
+        }
+    }
+
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            CntrCliente cntrCliente = new CntrCliente();
+
+            foreach (Cliente cliente in cntrCliente.ListaClientesAcimaDe20Anos())
+            {
+                Console.WriteLine(String.Format("{0} - {1} - {2}", cliente.Id, cliente.Nome, cliente.Idade));
+            }
+            Console.ReadKey();
         }
     }
 }
